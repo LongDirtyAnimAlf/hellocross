ec52fc4a7898154a682188d63d0f4ed2a8dd994d
Don <LongDirtyAnimAlf@gmail.com>
2020-11-30 22:11:08 +0100

    Vanilla

---
+++ main.pas
@@ -0,0 +1,39 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click({%H-}Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Edit1.Text:='Hello';
+end;
+
+end.
+
